199eae2ab41ce43b3eb34d6cd14836207f3d1e0b
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-04-30 16:00:02 +0300

    feat: add config files
    config files to determine which environment the app is running in
    "keys.ts" is the entry point for the config files,
    "dev.ts" holds the dev config,
    "prod.ts" holds the prod config,
    "tst.ts" holds the test config.
    all of these files export their keys from .env file

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -1,8 +1,4 @@
-import Configs from '../types/configs';
-
-const dev: Configs = {
-	PORT: process.env.PORT,
-	DB_URL: process.env.DEV_DB_URL,
-};
-
-export default dev;
+export default {
+    PORT: process.env.PORT,
+    DB_URL: process.env.DEV_DB_URL,
+}
