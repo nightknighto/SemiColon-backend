7b1b7ee393c3f509b8aff1e357ba338d32f24d83
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-05-01 18:29:49 +0300

    fix(services): call dotenv.config()
    in mongodb.ts file use config instead of deprecated keys

--- src/services/mongodb.ts
+++ src/services/mongodb.ts
@@ -1,27 +1,27 @@
 import mongoose from 'mongoose';
 
-import keys from '../config/keys';
+import configs from '../config/config';
 
-export async function connectMONGODB(){
-    try{
-        await mongoose.connect(keys.DB_URL);
-    } catch(err){
-        console.log(err);
-    }
+export async function connectMONGODB() {
+	try {
+		await mongoose.connect(configs.DB_URL as string);
+	} catch (err) {
+		console.log(err);
+	}
 }
 
-export async function disconnectMONGODB(){
-    try{
-        await mongoose.disconnect();
-    } catch(err){
-        console.log(err);
-    }
+export async function disconnectMONGODB() {
+	try {
+		await mongoose.disconnect();
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 mongoose.connection.on('connected', () => {
-    console.log('Connected to MongoDB');
+	console.log('Connected to MongoDB');
 });
 
 mongoose.connection.on('disconnected', () => {
-    console.log('Disconnected from MongoDB');
+	console.log('Disconnected from MongoDB');
 });
