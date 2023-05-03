af59f264e5f22143058fb10a2f3d4a044a397e58
Abdelrahman Mostafa <abdelrahman-mostafa@localhost.localdomain>
2023-05-04 02:59:09 +0300

    finalizing conflicts

--- src/api.ts
+++ src/api.ts
@@ -5,6 +5,13 @@ import globalRouter from "./routers/global.router";
 const api = express();
 
 api.use(express.json());
+
+api.use((req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 api.use("/", globalRouter);
 
 export default api;
