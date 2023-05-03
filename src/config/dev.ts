f055e4c616d041819bf14a8992a4749a96b7fff9
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-05-03 23:36:20 +0300

    feat: use env variable for session_secret

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -1,4 +1,5 @@
 export default {
     PORT: process.env.PORT,
     DB_URL: process.env.DEV_DB_URL,
+    SESSION_SECRET: process.env.SESSION_SECRET,
 }
--- src/services/cookie-session.ts
+++ src/services/cookie-session.ts
@@ -3,7 +3,7 @@ import configs from '../config/config';
 export default cookieSession({
 	name: 'semicolon',
 	maxAge: 24 * 60 * 60 * 1000, // 24 hours
-	secret: 'w@EeA1^Ic6kMAmQxd@u5zEv097xGc#59dznLod!87GaO%ISbFn', // TODO: user env variable from configs
+	secret: configs.SESSION_SECRET,
 	secure: false, // TODO: when https is available, set to true
 	httpOnly: true,
 });
