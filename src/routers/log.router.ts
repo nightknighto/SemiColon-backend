8200fdaa2760018b877837e4f49e8d5c53891fd7
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-05-06 02:14:52 +0300

    feat(router): protect log and participant routers

--- src/routers/log.router.ts
+++ src/routers/log.router.ts
@@ -1,10 +1,16 @@
-import {Router} from 'express';
-import { getAllLogs, getLogsByAdminId, getLogsByParticipantId } from '../controllers/log.controller';
+import { Router } from 'express';
+import {
+	getAllLogs,
+	getLogsByAdminId,
+	getLogsByParticipantId,
+} from '../controllers/log.controller';
+import isAdmin from '../middlewares/authentication/admin.middleware';
+import isLoggedIn from '../middlewares/authentication/login.middleware';
 
 const logRouter = Router();
 
-logRouter.get('/', getAllLogs);
-logRouter.get('/admin/:phone', getLogsByAdminId);
-logRouter.get('/participant/:id', getLogsByParticipantId)
+logRouter.get('/', isLoggedIn, isAdmin, getAllLogs);
+logRouter.get('/admin/:phone', isLoggedIn, isAdmin, getLogsByAdminId);
+logRouter.get('/participant/:id', isLoggedIn, isAdmin, getLogsByParticipantId);
 
 export default logRouter;
