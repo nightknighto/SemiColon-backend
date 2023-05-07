c1fa0441fb677dd03752dda8933363226321b9fe
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-05-08 00:37:52 +0300

    feat: apply giveAccess mw to all protected routes

--- src/routers/log.router.ts
+++ src/routers/log.router.ts
@@ -4,13 +4,23 @@ import {
 	getLogsByAdminId,
 	getLogsByParticipantId,
 } from '../controllers/log.controller';
-import isAdmin from '../middlewares/authentication/admin.middleware';
 import isLoggedIn from '../middlewares/authentication/login.middleware';
+import giveAccessTo from '../middlewares/authentication/giveAccessTo.middleware';
 
 const logRouter = Router();
 
-logRouter.get('/', isLoggedIn, isAdmin, getAllLogs);
-logRouter.get('/admin/:phone', isLoggedIn, isAdmin, getLogsByAdminId);
-logRouter.get('/participant/:id', isLoggedIn, isAdmin, getLogsByParticipantId);
+logRouter.get('/', isLoggedIn, giveAccessTo('admin'), getAllLogs);
+logRouter.get(
+	'/admin/:phone',
+	isLoggedIn,
+	giveAccessTo('admin'),
+	getLogsByAdminId
+);
+logRouter.get(
+	'/participant/:id',
+	isLoggedIn,
+	giveAccessTo('admin'),
+	getLogsByParticipantId
+);
 
 export default logRouter;
