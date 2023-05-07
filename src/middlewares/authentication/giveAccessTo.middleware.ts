a30e399f6581729def99bab06b43165d4e481384
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-05-08 01:35:44 +0300

    refactor: update giveAccessTo middleware
    update giveAccessTo middleware to use Role type instead of string
    Role type is an type derived from permissionLevels dynamically

--- src/middlewares/authentication/giveAccessTo.middleware.ts
+++ src/middlewares/authentication/giveAccessTo.middleware.ts
@@ -6,17 +6,19 @@ const permissionLevels = {
 	member: 1,
 };
 
+type Role = keyof typeof permissionLevels;
+
 // if the user has a role with a higher permission level than the route requires, then he can access it
-export default function giveAccessTo(role: 'admin' | 'hr' | 'member') {
+export default function giveAccessTo(role: Role) {
 	return (req: Request, res: Response, next: NextFunction) => {
-		let userRole = req.user?.role as 'admin' | 'hr' | 'member';
+		let userRole = req.user?.role as Role;
 		let isActive = req.user?.active as boolean;
 		if (permissionLevels[userRole] >= permissionLevels[role] && isActive) {
 			next();
 		} else {
 			res.status(401).send({
 				status: 'failure',
-				message: `You must be at least an ** ${role} to access this route`,
+				message: `You must be at least an ACTIVE ${role} to access this route`,
 			});
 		}
 	};
