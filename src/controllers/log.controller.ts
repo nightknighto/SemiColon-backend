540de08cddc5d894aae52ca2515eb9bf8ad9bcd1
Omar Fahmy <Ofahmy1234@gmail.com>
2023-05-09 05:08:37 +0300

    feat: participants schema change - trim participants object at request start - remove required from firstPrefReason, secondPrefReason and pastExperience

--- src/controllers/log.controller.ts
+++ src/controllers/log.controller.ts
@@ -1,70 +1,70 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 import {
-	dbGetAllLogs,
-	dbGetLogsByAdminId,
-	dbGetLogsByParticipantId,
-} from '../models/log/log.model';
-import ErrorWithStatusCode from '../util/classes/ErrorWithStatusCode';
+    dbGetAllLogs,
+    dbGetLogsByAdminId,
+    dbGetLogsByParticipantId,
+} from "../models/log/log.model";
+import ErrorWithStatusCode from "../utils/classes/ErrorWithStatusCode";
 
 export async function getAllLogs(req: Request, res: Response) {
-	try {
-		const allLogs = await dbGetAllLogs();
-		res.status(200).json({
-			status: 'success',
-			data: allLogs,
-		});
-	} catch (error: unknown) {
-		res.status((error as ErrorWithStatusCode).statusCode || 500).json({
-			status: 'failure',
-			message: (error as Error).message,
-		});
-	}
+    try {
+        const allLogs = await dbGetAllLogs();
+        res.status(200).json({
+            status: "success",
+            data: allLogs,
+        });
+    } catch (error: unknown) {
+        res.status((error as ErrorWithStatusCode).statusCode || 500).json({
+            status: "failure",
+            message: (error as Error).message,
+        });
+    }
 }
 
 export async function getLogsByAdminId(req: Request, res: Response) {
-	try {
-		const adminId = req.params.id;
-		const logs = await dbGetLogsByAdminId(adminId);
-		res.status(200).json({
-			status: 'success',
-			data: logs,
-		});
-	} catch (error: unknown) {
-		res.status((error as ErrorWithStatusCode).statusCode || 500).json({
-			status: 'failure',
-			message: (error as Error).message,
-		});
-	}
+    try {
+        const adminId = req.params.id;
+        const logs = await dbGetLogsByAdminId(adminId);
+        res.status(200).json({
+            status: "success",
+            data: logs,
+        });
+    } catch (error: unknown) {
+        res.status((error as ErrorWithStatusCode).statusCode || 500).json({
+            status: "failure",
+            message: (error as Error).message,
+        });
+    }
 }
 
 export async function getLogsByParticipantId(req: Request, res: Response) {
-	try {
-		const participantId = req.params.id;
-		const logs = await dbGetLogsByParticipantId(participantId);
-		res.status(200).json({
-			status: 'success',
-			data: logs,
-		});
-	} catch (error: unknown) {
-		res.status((error as ErrorWithStatusCode).statusCode || 500).json({
-			status: 'failure',
-			message: (error as Error).message,
-		});
-	}
+    try {
+        const participantId = req.params.id;
+        const logs = await dbGetLogsByParticipantId(participantId);
+        res.status(200).json({
+            status: "success",
+            data: logs,
+        });
+    } catch (error: unknown) {
+        res.status((error as ErrorWithStatusCode).statusCode || 500).json({
+            status: "failure",
+            message: (error as Error).message,
+        });
+    }
 }
 
 export async function getLogsByAdminPhone(req: Request, res: Response) {
-	try {
-		const adminPhone = req.params.phone;
-		const logs = await dbGetLogsByAdminId(adminPhone);
-		res.status(200).json({
-			status: 'success',
-			data: logs,
-		});
-	} catch (error: unknown) {
-		res.status((error as ErrorWithStatusCode).statusCode || 500).json({
-			status: 'failure',
-			message: (error as Error).message,
-		});
-	}
+    try {
+        const adminPhone = req.params.phone;
+        const logs = await dbGetLogsByAdminId(adminPhone);
+        res.status(200).json({
+            status: "success",
+            data: logs,
+        });
+    } catch (error: unknown) {
+        res.status((error as ErrorWithStatusCode).statusCode || 500).json({
+            status: "failure",
+            message: (error as Error).message,
+        });
+    }
 }
