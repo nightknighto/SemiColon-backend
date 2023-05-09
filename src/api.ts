eeb9136b0a666b405a7c48cd535782e0d53d206d
Abdelrahman Mostafa <abdelrahman-mostafa@localhost.localdomain>
2023-05-10 02:34:32 +0300

    adding final touches

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,11 @@
-import express from 'express';
-import passport from 'passport';
-import cookieSession from './services/cookie-session';
-import { serve, setup } from 'swagger-ui-express';
-import globalRouter from './routers/global.router';
-import './services/passport';
-import cors from './middlewares/security/cors.middleware';
+import express from "express";
+import passport from "passport";
+import cookieSession from "./services/cookie-session";
+import { serve, setup } from "swagger-ui-express";
+import globalRouter from "./routers/global.router";
+import "./services/passport";
+import cors from "./middlewares/security/cors.middleware";
+const swaggerDoc = require("./swagger_output.json");
 
 const api = express();
 
@@ -13,6 +14,7 @@ api.use(cookieSession);
 api.use(passport.initialize());
 api.use(passport.session());
 api.use(cors);
-api.use('/', globalRouter);
+api.use("/doc", serve, setup(swaggerDoc));
+api.use("/", globalRouter);
 
 export default api;
