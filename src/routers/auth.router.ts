2ebea98e0aaec4c11566710d4648382fff12400d
Abdelrahman Mostafa <71796506+bobo122b@users.noreply.github.com>
2023-05-12 16:46:27 +0300

    Adding swagger (#14)
    * Adding latest swagger functionality
    
    * Adding swagger User tag
    
    * Adding swagger to authentication routes
    
    * Adding swagger to participants routes
    
    * Adding swagger to log routers
    
    * adding latest package.json
    
    * Adding swagger link as a comment for next projects
    
    ---------
    
    Co-authored-by: Abdelrahman Mostafa <abdelrahman-mostafa@localhost.localdomain>

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,18 +1,24 @@
-import { Router } from 'express';
-import passport from 'passport';
-import { failedLogin, successfulLogin } from '../controllers/auth.controller';
+import { Router } from "express";
+import passport from "passport";
+import { failedLogin, successfulLogin } from "../controllers/auth.controller";
 const authRouter = Router();
 
 authRouter.post(
-	'/login',
-	passport.authenticate('local', {
-		successRedirect: '/auth/login/success',
-		failureRedirect: '/auth/login/failure',
-	})
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/auth/login/success",
+    failureRedirect: "/auth/login/failure",
+  }),
+  () => {
+    /**
+     * #swagger.tags = ['Auth']
+     * #swagger.description = 'Endpoint to authenticate All logins login'
+     */
+  }
 );
 
-authRouter.route('/login/success').get(successfulLogin).post(successfulLogin);
+authRouter.route("/login/success").get(successfulLogin).post(successfulLogin);
 
-authRouter.route('/login/failure').get(failedLogin).post(failedLogin);
+authRouter.route("/login/failure").get(failedLogin).post(failedLogin);
 
 export default authRouter;
--- src/routers/log.router.ts
+++ src/routers/log.router.ts
@@ -1,26 +1,27 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
-	getAllLogs,
-	getLogsByAdminId,
-	getLogsByParticipantId,
-} from '../controllers/log.controller';
-import isLoggedIn from '../middlewares/authentication/login.middleware';
-import giveAccessTo from '../middlewares/authentication/giveAccessTo.middleware';
+  getAllLogs,
+  getLogsByAdminId,
+  getLogsByAdminPhone,
+  getLogsByParticipantId,
+} from "../controllers/log.controller";
+import isLoggedIn from "../middlewares/authentication/login.middleware";
+import giveAccessTo from "../middlewares/authentication/giveAccessTo.middleware";
 
 const logRouter = Router();
 
-logRouter.get('/', isLoggedIn, giveAccessTo('admin'), getAllLogs);
+logRouter.get("/", isLoggedIn, giveAccessTo("admin"), getAllLogs);
 logRouter.get(
-	'/admin/:phone',
-	isLoggedIn,
-	giveAccessTo('admin'),
-	getLogsByAdminId
+  "/admin/:phone",
+  isLoggedIn,
+  giveAccessTo("admin"),
+  getLogsByAdminPhone
 );
 logRouter.get(
-	'/participant/:id',
-	isLoggedIn,
-	giveAccessTo('admin'),
-	getLogsByParticipantId
+  "/participant/:id",
+  isLoggedIn,
+  giveAccessTo("admin"),
+  getLogsByParticipantId
 );
 
 export default logRouter;
