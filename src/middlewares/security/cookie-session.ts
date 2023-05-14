3c7ad16496e2aa0f079f4954dc3c9769ef6e91c0
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-05-15 01:41:23 +0300

    fix: cookie-session middleware
    set secure to false for development

--- src/middlewares/security/cookie-session.ts
+++ src/middlewares/security/cookie-session.ts
@@ -4,7 +4,7 @@ export default cookieSession({
 	name: 'semicolon',
 	maxAge: 24 * 60 * 60 * 1000, // 24 hours
 	secret: configs.SESSION_SECRET,
-	secure: true, // Send the cookie over https only
+	secure: false, // Send the cookie over https only
 	httpOnly: true, // Disable accessing the cookie from client side js
 	sameSite: 'none', // Send the cookies in cross site requests
 });
