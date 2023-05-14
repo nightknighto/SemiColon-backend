693b3e526f9947b9c80b293e18b9eb89c265b06c
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-05-15 01:56:04 +0300

    Expose participants route (#17)
    * feat: add logging to the api
    
    * feat: update cookie-session middleware
    
    set sameSite to 'none' to enable send the cookies in cross origin
    requests
    set secure to true to send the cookies only over https
    
    * feat: add new script to package.json
    
    add gen-cert script to generate a self-signed certificate to test
    https locally
    
    * feat: update cors middleware
    
    add front ends link to the allowed list
    
    * expose participants GET route
    
    * fix: cookie-session middleware
    
    set secure to false for development
    
    ---------
    
    Co-authored-by: Abdelrahman Mostafa <abdelrahman-mostafa@localhost.localdomain>

---
+++ src/middlewares/security/cookie-session.ts
@@ -0,0 +1,10 @@
+import cookieSession from 'cookie-session';
+import configs from '../../config/config';
+export default cookieSession({
+	name: 'semicolon',
+	maxAge: 24 * 60 * 60 * 1000, // 24 hours
+	secret: configs.SESSION_SECRET,
+	secure: false, // Send the cookie over https only
+	httpOnly: true, // Disable accessing the cookie from client side js
+	sameSite: 'none', // Send the cookies in cross site requests
+});
--- src/middlewares/security/cors.middleware.ts
+++ src/middlewares/security/cors.middleware.ts
@@ -1,12 +1,24 @@
-import cors from "cors";
+import cors, { CorsOptions } from 'cors';
+import { Request } from 'express';
 
 /* 
 	CORS (Cross-Origin Resource Sharing) is a security feature implemented in web browsers that aims to
 	protect the user (client) from malicious websites by preventing unauthorized cross-origin requests from
 	being made by scripts running in the browser.
 */
-const corsOptions = {
-  origin: "*",
+const allowlist = [
+	'http://127.0.0.1:5500',
+	'https://deadreyo.github.io/SemiColon-dashboard-frontend',
+	'https://asusemicolon.tech',
+];
+let corsOptionsDelegate = function (req: Request, callback: Function) {
+	let corsOptions: CorsOptions = { credentials: true };
+	if (allowlist.indexOf(req.header('Origin') as string) !== -1) {
+		corsOptions.origin = true; // if origin exist on the allowList
+	} else {
+		corsOptions.origin = false; // disable CORS for this request
+	}
+	callback(null, corsOptions); // callback expects two parameters: error and options
 };
 
-export default cors(corsOptions);
+export default cors(corsOptionsDelegate);
