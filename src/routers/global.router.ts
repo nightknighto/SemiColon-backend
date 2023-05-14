cef0f78e9a85e1e2ed53631334371bf08a2a43e5
Abdelrahman Mostafa <71796506+bobo122b@users.noreply.github.com>
2023-05-14 22:04:24 +0300

    Bobo (#18)
    * resolving some errors and changing corsOptions
    
    * Adding latest swagger changes
    
    * resolving a tiny issue
    
    ---------
    
    Co-authored-by: Abdelrahman Mostafa <abdelrahman-mostafa@localhost.localdomain>

--- src/routers/global.router.ts
+++ src/routers/global.router.ts
@@ -18,7 +18,7 @@ globalRouter.use("/log", logRouter);
 globalRouter.use(
   "/",
   serve,
-  setup(swaggerDoc, { swaggerUrl: process.env.PORT })
+  setup(swaggerDoc, { swaggerUrl: process.env.PORT, explorer: true })
 );
 
 export default globalRouter;
