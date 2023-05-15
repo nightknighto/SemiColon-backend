78a6b1f52dcce200de667ac818739c47be569197
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-05-15 19:59:40 +0300

    Revert "Expose participants route (#17)"
    This reverts commit 693b3e526f9947b9c80b293e18b9eb89c265b06c.

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import passport from 'passport';
-import cookieSession from './middlewares/security/cookie-session';
+import cookieSession from './services/cookie-session';
 
 import globalRouter from './routers/global.router';
 import './services/passport';
--- src/middlewares/security/cors.middleware.ts
+++ src/middlewares/security/cors.middleware.ts
@@ -1,24 +1,12 @@
-import cors, { CorsOptions } from 'cors';
-import { Request } from 'express';
+import cors from "cors";
 
 /* 
 	CORS (Cross-Origin Resource Sharing) is a security feature implemented in web browsers that aims to
 	protect the user (client) from malicious websites by preventing unauthorized cross-origin requests from
 	being made by scripts running in the browser.
 */
-const allowlist = [
-	'http://127.0.0.1:5500',
-	'https://deadreyo.github.io/SemiColon-dashboard-frontend',
-	'https://asusemicolon.tech',
-];
-let corsOptionsDelegate = function (req: Request, callback: Function) {
-	let corsOptions: CorsOptions = { credentials: true };
-	if (allowlist.indexOf(req.header('Origin') as string) !== -1) {
-		corsOptions.origin = true; // if origin exist on the allowList
-	} else {
-		corsOptions.origin = false; // disable CORS for this request
-	}
-	callback(null, corsOptions); // callback expects two parameters: error and options
+const corsOptions = {
+  origin: "*",
 };
 
-export default cors(corsOptionsDelegate);
+export default cors(corsOptions);
