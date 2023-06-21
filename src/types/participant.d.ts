3a86b1b2ceab103d977a273c7288d6949144dd02
Omar Fahmy <Ofahmy1234@gmail.com>
2023-06-22 00:42:30 +0300

    feat: interviewer note type

--- src/types/participant.d.ts
+++ src/types/participant.d.ts
@@ -14,7 +14,12 @@ export interface participant {
     secondPrefReason: string;
     pastExperience: string;
     acceptanceStatus: string;
-    emailedStatus: boolean;
+    InterviewerNote: string;
+}
+
+export interface InterviewerNote {
+    rating: 1|2|3|4|5;
+    note: string;
 }
 // this type mean only one field of the above type can be used as a filter to update at a time
 type UpdateParticipant = {
