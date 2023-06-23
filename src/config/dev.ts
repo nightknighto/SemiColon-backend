c1ac72d08c35bc6a4608d1e86f2d6a061210603b
Omar Fahmy <Ofahmy1234@gmail.com>
2023-06-23 17:19:02 +0300

    feat: add hr mail credentials

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -2,4 +2,6 @@ export default {
     PORT: process.env.PORT,
     DB_URL: process.env.DEV_DB_URL,
     SESSION_SECRET: process.env.SESSION_SECRET,
+    MAIL_USER: process.env.PROD_MAIL_USER,
+    MAIL_APP_PASSWORD: process.env.PROD_MAIL_APP_PASSWORD,
 }
