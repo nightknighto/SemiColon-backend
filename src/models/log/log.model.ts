41b3c6e6a88fd7fba6763cc2b2e57fdd6ef97a60
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-06-27 13:26:27 +0300

    feat: update logs model
    update logs model to use the session object to perform
    transactions

--- src/models/log/log.model.ts
+++ src/models/log/log.model.ts
@@ -1,25 +1,26 @@
+import mongoose from "mongoose";
 import { UserLogType, ParticipantLogType } from "../../types/log";
 import { UserLog, ParticipantLog } from "./log.schema";
 
-export async function dbAddNewUserLog(log: UserLogType) {
-	await UserLog.create(log);
+export async function dbAddNewUserLog(log: UserLogType, session?: mongoose.ClientSession) {
+	await UserLog.create([log], { session });
 }
 
-export async function dbAddNewParticipantLog(log: ParticipantLogType) {
-	await ParticipantLog.create(log);
+export async function dbAddNewParticipantLog(log: ParticipantLogType, session?: mongoose.ClientSession) {
+	await ParticipantLog.create([log], { session });
 }
 
-export async function dbGetAllParticipantLogs() {
-	return await ParticipantLog.find({}).populate("initiatorId").populate("targetId");
+export async function dbGetAllParticipantLogs(session?: mongoose.ClientSession) {
+	return await ParticipantLog.find({}, {}, { session }).populate("initiatorId").populate("targetId");
 }
 
-export async function dbGetAllUserLogs() {
-	return await UserLog.find({}).populate("initiator").populate("target");
+export async function dbGetAllUserLogs(session?: mongoose.ClientSession) {
+	return await UserLog.find({}, {}, { session }).populate("initiator").populate("target");
 }
 
-export async function dbGetLogsByAdminId(adminId: string) {
+export async function dbGetLogsByAdminId(adminId: string, session?: mongoose.ClientSession) {
 	const userLogs = await UserLog.find({ initiatorId: adminId }).populate("initiator").populate("target");
-	const participantLogs = await ParticipantLog.find({ initiatorId: adminId })
+	const participantLogs = await ParticipantLog.find({ initiatorId: adminId }, {}, { session })
 		.populate("initiator")
 		.populate("target");
 	return {
@@ -28,9 +29,9 @@ export async function dbGetLogsByAdminId(adminId: string) {
 	};
 }
 
-export async function dbGetLogsByAdminPhone(adminPhone: string) {
+export async function dbGetLogsByAdminPhone(adminPhone: string, session?: mongoose.ClientSession) {
 	const userLogs = await UserLog.find({ initiatorPhone: adminPhone }).populate("initiator").populate("target");
-	const participantLogs = await ParticipantLog.find({ initiatorPhone: adminPhone })
+	const participantLogs = await ParticipantLog.find({ initiatorPhone: adminPhone }, {}, { session })
 		.populate("initiator")
 		.populate("target");
 	return {
@@ -39,6 +40,8 @@ export async function dbGetLogsByAdminPhone(adminPhone: string) {
 	};
 }
 
-export async function dbGetLogsByParticipantId(participantId: string) {
-	return await ParticipantLog.find({ targetId: participantId }).populate("initiator").populate("target");
+export async function dbGetLogsByParticipantId(participantId: string, session?: mongoose.ClientSession) {
+	return await ParticipantLog.find({ targetId: participantId }, {}, { session })
+		.populate("initiator")
+		.populate("target");
 }
