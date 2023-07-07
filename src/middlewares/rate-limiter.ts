9306dd5bd482220b8e047c8e0fa13c6090548494
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2023-07-07 18:25:39 +0300

    changing rate limit

--- src/middlewares/rate-limiter.ts
+++ src/middlewares/rate-limiter.ts
@@ -1,7 +1,7 @@
 import rateLimiter from "express-rate-limit";
 
 export const limiter = rateLimiter({
-  max: 5,
+  max: 100,
   windowMs: 5000,
   message: "You can't make any more requests at the moment. Try again later",
 });
