d5246fa54ebc058aecc65e80a6039dc01b9ef6c3
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-07-14 19:16:13 +0300

    fix: fix interview notes schema

--- src/models/participant/participant.schema.ts
+++ src/models/participant/participant.schema.ts
@@ -32,17 +32,18 @@ function createInterviewerNoteSchema(criteria: string[]) {
     const criteriaSchema: any = {};
     for (const criterion of criteria) {
         criteriaSchema[criterion] = {
-            rating: {
-                type: Number,
-                enum: [1, 2, 3, 4, 5],
-                default: 1,
+            type: {
+                rating: {
+                    type: Number,
+                    enum: [1, 2, 3, 4, 5],
+                    required: true,
+                },
+                note: {
+                    type: String,
+                    trim: true,
+                }
             },
-            note: {
-                type: String,
-                default: "",
-                trim: true,
-            
-            },
+            required: false,
         };
     }
     const interviewerObjectSchema = {
