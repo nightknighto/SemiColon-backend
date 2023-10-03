1c6321b7ba4737a3ab4452d6d62265b1b1b31665
Omar Fahmy <Ofahmy1234@gmail.com>
2023-10-04 00:15:34 +0300

    feat: add user commitees in user schema

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,11 +1,20 @@
-import { userSchema } from '../models/user/user.schema';
-import { Mongoose } from 'mongoose';
+import { userSchema } from '../models/user/user.schema'
+import { Mongoose } from 'mongoose'
 
+export enum CommitteeEnum {
+    WEB_ADVANCED = 'web-advanced',
+    WEB_BASIC = 'embedded',
+    AVR = 'avr',
+    ARM = 'arm',
+    EMBEDDED_C = 'embedded-c',
+    PYTHON = 'python',
+}
 export default interface UserType {
-	_id?: Mongoose.Types.ObjectId;
-	username: string;
-	password: string;
-	phone: string;
-	role: string;
-	active?: boolean;
+    _id?: Mongoose.Types.ObjectId
+    username: string
+    password: string
+    phone: string
+    committee: CommitteeEnum
+    role: string
+    active?: boolean
 }
