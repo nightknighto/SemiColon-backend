148d83761119a18e975fe3168582d40607c708d6
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-10-13 23:18:46 +0300

    feat: add authorization to applicant router

--- src/routers/applicant.router.ts
+++ src/routers/applicant.router.ts
@@ -2,14 +2,22 @@ import { Router } from 'express'
 import {
     addNewApplicant,
     getAllApplicants,
+    getApplicantById,
     updateApplicantById,
 } from '../controllers/applicant.controller'
+import isLoggedIn from '../middlewares/authentication/login.middleware'
+import giveAccessTo from '../middlewares/authentication/giveAccessTo.middleware'
 
 const applicantRouter = Router()
 
-applicantRouter.get('/', getAllApplicants)
+applicantRouter.get('/', isLoggedIn, getAllApplicants)
+applicantRouter.get('/:id', isLoggedIn, getApplicantById)
 applicantRouter.post('/', addNewApplicant)
-applicantRouter.patch('/:id', updateApplicantById)
-applicantRouter.get('/:id', updateApplicantById)
+applicantRouter.patch(
+    '/:id',
+    isLoggedIn,
+    giveAccessTo('hr'),
+    updateApplicantById,
+)
 
 export default applicantRouter
