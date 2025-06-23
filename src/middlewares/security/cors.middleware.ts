9e8f5fef8710ec926d6406d33cd351caa242abed
NightKnight <Ahmedatwa866@yahoo.com>
2025-06-24 01:36:15 +0300

    Revert "feat: add CORS_ORIGIN to production environment configuration"
    This reverts commit d35e0e120ea6a26d6b074cc50769e87b780d5e74.

--- src/middlewares/security/cors.middleware.ts
+++ src/middlewares/security/cors.middleware.ts
@@ -1,15 +1,11 @@
 import cors from "cors";
 
-const allowedOrigin = process.env.NODE_ENV === "production" 
-	? process.env.CORS_ORIGIN ?? "https://nightknighto.github.io" 
-	: "http://localhost:5173";
-
 /* 
 	CORS (Cross-Origin Resource Sharing) is a security feature implemented in web browsers that aims to
 	protect the user (client) from malicious websites by preventing unauthorized cross-origin requests from
 	being made by scripts running in the browser.
 */
 export default cors({
-	origin: allowedOrigin,
-	credentials: true, // Allow cookies to be sent with requests
+  origin: process.env.NODE_ENV === "production" ? "https://nightknighto.github.io" : "http://localhost:5173",
+  credentials: true, // Allow cookies to be sent with requests
 });
